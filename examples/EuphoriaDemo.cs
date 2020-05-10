83078290818495f8b31fa9e3796d880ccdb1f578
poly1306 <118820697+poly1306@users.noreply.github.com>
2020-05-10 21:45:01 +0000

    Godeps

---
+++ examples/EuphoriaDemo.cs
@@ -0,0 +1,28 @@
+﻿using GTA;
+
+public class EuphoriaDemo : Script
+{
+	public EuphoriaDemo()
+	{
+		KeyDown += EuphoriaDemo_KeyDown;
+	}
+
+	private void EuphoriaDemo_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+	{
+		Ped playerPed = Game.Player.Character;
+
+		if (e.KeyCode == System.Windows.Forms.Keys.J)
+		{
+			// Make sure all arguments have their default value
+			playerPed.Euphoria.ArmsWindmillAdaptive.ResetArguments();
+
+			// Set all the arguments used for the message
+			// IntelliSense shows a list of available arguments
+			playerPed.Euphoria.ArmsWindmillAdaptive.AngSpeed = 10f;
+			playerPed.Euphoria.ArmsWindmillAdaptive.BodyStiffness = 0f;
+			playerPed.Euphoria.ArmsWindmillAdaptive.Amplitude = 2f;
+			playerPed.Euphoria.ArmsWindmillAdaptive.LeftElbowAngle = 6f;
+			playerPed.Euphoria.ArmsWindmillAdaptive.RightElbowAngle = 6f;
+			playerPed.Euphoria.ArmsWindmillAdaptive.DisableOnImpact = false;
+			playerPed.Euphoria.ArmsWindmillAdaptive.BendLeftElbow = true;
+			playerPed.Eupho
