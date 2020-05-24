4e6d024574be2e0e827869d2aad1d120254c7ede
poly1306 <118820697+poly1306@users.noreply.github.com>
2020-05-24 22:18:50 +0000

    Commit para sincronización entre equipos

--- source/core/NativeFunc.cs
+++ source/core/NativeFunc.cs
@@ -5,4 +5,77 @@
 
 using System;
 using System.Runtime.InteropServices;
-using System.Se
+using System.Security;
+using System.Text;
+
+namespace SHVDN
+{
+	/// <summary>
+	/// Class responsible for executing script functions.
+	/// </summary>
+	public static unsafe class NativeFunc
+	{
+		#region ScriptHookV Imports
+		/// <summary>
+		/// Initializes the stack for a new script function call.
+		/// </summary>
+		/// <param name="hash">The function hash to call.</param>
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport("ScriptHookV.dll", ExactSpelling = true, EntryPoint = "?nativeInit@@YAX_K@Z")]
+		static extern void NativeInit(ulong hash);
+
+		/// <summary>
+		/// Pushes a function argument on the script function stack.
+		/// </summary>
+		/// <param name="val">The argument value.</param>
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport("ScriptHookV.dll", ExactSpelling = true, EntryPoint = "?nativePush64@@YAX_K@Z")]
+		static extern void NativePush64(ulong val);
+
+		/// <summary>
+		/// Executes the script function call.
+		/// </summary>
+		/// <returns>A pointer to the return value of the call.</returns>
+		[SuppressUnmanagedCodeSecurity]
+		[DllImport("ScriptHookV.dll", ExactSpelling = true, EntryPoint = "?nativeCall@@YAPEA_KXZ")]
+		static unsafe extern ulong* NativeCall();
+		#endregion
+
+		/// <summary>
+		/// Internal script task which holds all data necessary for a script function call.
+		/// </summary>
+		class NativeTask : IScriptTask
+		{
+			internal ulong Hash;
+			internal ulong[] Arguments;
+			internal unsafe ulong* Result;
+
+			public void Run()
+			{
+				Result = InvokeInternal(Hash, Arguments);
+			}
+		}
+
+		/// <summary>
+		/// Internal script task which holds all data necessary for a script function call.
+		/// </summary>
+		class NativeTaskPtrArgs : IScriptTask
+		{
+			internal ulong Hash;
+			internal ulong* ArgumentPtr;
+			internal int ArgumentCount;
+			internal unsafe ulong* Result;
+
+			public void Run()
+			{
+				Result = InvokeInternal(Hash, ArgumentPtr, ArgumentCount);
+			}
+		}
+
+		/// <summary>
+		/// Pushes a single string component on the text stack.
+		/// </summary>
+		/// <param name="str">The string to push.</param>
+		static void PushString(string str)
+		{
+			var domain = 
