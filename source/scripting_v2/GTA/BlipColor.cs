3583522fe70235eee023a57e73e49d4a261cccaa
poly1306 <118820697+poly1306@users.noreply.github.com>
2020-06-21 20:53:56 +0000

    Ready

---
+++ source/scripting_v2/GTA/BlipColor.cs
@@ -0,0 +1,19 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+namespace GTA
+{
+	public enum BlipColor
+	{
+		/// <summary>
+		/// The default RGB value of this color is the same as HUD_COLOUR_PURE_WHITE, whose default RGB value is #FFFFFF.
+		/// </summary>
+		White,
+		Red,
+		Green,
+		Blue,
+		Yellow = 66,
+		/// <summary>
+		/// The default RGB value of this color is the same as HUD_COLOUR_WHITE, 
