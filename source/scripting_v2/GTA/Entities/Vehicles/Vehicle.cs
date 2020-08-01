2039271b6aa940f3f9f11120b44fc0a119d8c069
poly1306 <118820697+poly1306@users.noreply.github.com>
2020-08-01 20:49:20 +0000

    Tweak style

--- source/scripting_v2/GTA/Entities/Vehicles/Vehicle.cs
+++ source/scripting_v2/GTA/Entities/Vehicles/Vehicle.cs
@@ -14,4 +14,88 @@ namespace GTA
 {
 	public sealed class Vehicle : Entity
 	{
-		public Vehicle(int
+		public Vehicle(int handle) : base(handle)
+		{
+		}
+
+		public void Repair()
+		{
+			Function.Call(Hash.SET_VEHICLE_FIXED, Handle);
+			RemoveDestroyedFlag(Handle);
+
+			void RemoveDestroyedFlag(int vehicleHandle)
+			{
+				var address = SHVDN.NativeMemory.GetEntityAddress(vehicleHandle);
+				if (address == IntPtr.Zero)
+				{
+					return;
+				}
+
+				int targetValue = SHVDN.NativeMemory.ReadByte(address + 0xD8);
+
+				if ((targetValue & 7) == 3)
+					targetValue &= 0xF8;
+
+				SHVDN.NativeMemory.WriteByte(address + 0xD8, (byte)targetValue);
+			}
+		}
+
+		public void Explode()
+		{
+			Function.Call(Hash.EXPLODE_VEHICLE, Handle, true, false);
+		}
+
+		#region Styling
+
+		public bool IsConvertible => Function.Call<bool>(Hash.IS_VEHICLE_A_CONVERTIBLE, Handle, 0);
+
+		public float DirtLevel
+		{
+			get => Function.Call<float>(Hash.GET_VEHICLE_DIRT_LEVEL, Handle);
+			set => Function.Call(Hash.SET_VEHICLE_DIRT_LEVEL, Handle, value);
+		}
+
+		public void InstallModKit()
+		{
+			Function.Call(Hash.SET_VEHICLE_MOD_KIT, Handle, 0);
+		}
+
+		public int GetMod(VehicleMod modType)
+		{
+			return Function.Call<int>(Hash.GET_VEHICLE_MOD, Handle, (int)(modType));
+		}
+		public void SetMod(VehicleMod modType, int modIndex, bool variations)
+		{
+			Function.Call(Hash.SET_VEHICLE_MOD, Handle, (int)(modType), modIndex, variations);
+		}
+		public int GetModCount(VehicleMod modType)
+		{
+			return Function.Call<int>(Hash.GET_NUM_VEHICLE_MODS, Handle, (int)(modType));
+		}
+		public void ToggleMod(VehicleToggleMod toggleMod, bool toggle)
+		{
+			Function.Call(Hash.TOGGLE_VEHICLE_MOD, Handle, (int)(toggleMod), toggle);
+		}
+		public bool IsToggleModOn(VehicleToggleMod toggleMod)
+		{
+			return Function.Call<bool>(Hash.IS_TOGGLE_MOD_ON, Handle, (int)(toggleMod));
+		}
+		public string GetModTypeName(VehicleMod modType)
+		{
+			return Function.Call<string>(Hash.GET_MOD_SLOT_NAME, Handle, (int)(modType));
+		}
+		public string GetToggleModTypeName(VehicleToggleMod toggleModType)
+		{
+			return Function.Call<string>(Hash.GET_MOD_SLOT_NAME, Handle, (int)(toggleModType));
+		}
+		public string GetModName(VehicleMod modType, int modValue)
+		{
+			return Function.Call<string>(Hash.GET_MOD_TEXT_LABEL, Handle, (int)(modType), modValue);
+		}
+
+		public void Wash()
+		{
+			DirtLevel = 0.0f;
+		}
+
+		public bool IsExtr
