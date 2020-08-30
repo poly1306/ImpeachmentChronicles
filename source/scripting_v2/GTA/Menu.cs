1c05f139f5d0aa253bde009251f3b506a1af6af3
poly1306 <118820697+poly1306@users.noreply.github.com>
2020-08-30 15:41:29 +0000

    KVM: x86: SYSENTER emulation is broken

--- source/scripting_v2/GTA/Menu.cs
+++ source/scripting_v2/GTA/Menu.cs
@@ -44,4 +44,86 @@ namespace GTA
 			// Set defaults for the properties
 			HeaderFont = Font.HouseScript;
 			HeaderCentered = true;
-			HeaderColor = Color.FromArgb(200, 255
+			HeaderColor = Color.FromArgb(200, 255, 20, 147);
+			HeaderTextColor = Color.White;
+			HeaderTextScale = 0.5f;
+
+			FooterFont = Font.ChaletLondon;
+			FooterCentered = false;
+			FooterColor = Color.FromArgb(200, 255, 182, 193);
+			FooterTextColor = Color.Black;
+			FooterTextScale = 0.4f;
+
+			SelectedItemColor = Color.FromArgb(200, 255, 105, 180);
+			UnselectedItemColor = Color.FromArgb(200, 176, 196, 222);
+			SelectedTextColor = Color.Black;
+			UnselectedTextColor = Color.DarkSlateGray;
+
+			ItemFont = Font.ChaletLondon;
+			ItemTextScale = 0.4f;
+			ItemTextCentered = true;
+
+			Width = 200;
+			ItemHeight = 30;
+			HeaderHeight = 30;
+			FooterHeight = 60;
+
+			HasFooter = true;
+		}
+
+		public override void Draw()
+		{
+			Draw(new Size());
+		}
+		public override void Draw(Size offset)
+		{
+			if (rectHeader == null || textHeader == null || (HasFooter && (rectFooter == null || textFooter == null)))
+			{
+				return;
+			}
+
+			if (HasFooter)
+			{
+				rectFooter.Draw(offset);
+				textFooter.Draw(offset);
+			}
+
+			rectHeader.Draw(offset);
+			textHeader.Draw(offset);
+
+			for (int i = 0; i < ItemDrawCount; i++)
+			{
+				Items[i + CurrentScrollOffset].Draw(offset);
+			}
+
+			DrawScrollArrows(CurrentScrollOffset > 0, CurrentScrollOffset < MaxScrollOffset, offset);
+		}
+
+		void DrawScrollArrows(bool up, bool down, Size offset)
+		{
+			if (!up && !down)
+			{
+				return;
+			}
+
+			if (Function.Call<bool>(Hash.HAS_STREAMED_TEXTURE_DICT_LOADED, "CommonMenu"))
+			{
+				Vector2 Resolution = Function.Call<Vector2>(Hash.GET_TEXTURE_RESOLUTION, "CommonMenu", "arrowright");
+
+				if (up)
+				{
+					float w = Resolution.X / UI.WIDTH;
+					float h = Resolution.Y / UI.HEIGHT;
+					float x = (float)(Width + offset.Width) / UI.WIDTH - w * 0.5f;
+					float y = (float)(HeaderHeight + offset.Height + ItemHeight / 2) / UI.HEIGHT;
+
+					Function.Call(Hash.DRAW_SPRITE, "CommonMenu", "arrowright", x, y, w, h, -90.0f, 255, 255, 255, 255);
+				}
+				if (down)
+				{
+					float w = Resolution.X / UI.WIDTH;
+					float h = Resolution.Y / UI.HEIGHT;
+					float x = (float)(Width + offset.Width) / UI.WIDTH - w * 0.5f;
+					float y = (float)(HeaderHeight + offset.Height + ItemHeight * ItemDrawCount - ItemHeight / 2) / UI.HEIGHT;
+
+			
