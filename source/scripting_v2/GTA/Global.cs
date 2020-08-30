0ef0ac3d0580dadb56b70d6c5a7ec19770d891b9
poly1306 <118820697+poly1306@users.noreply.github.com>
2020-08-30 07:38:54 +0000

    CVS_SILENT i18n style guide fixes

--- source/scripting_v2/GTA/Global.cs
+++ source/scripting_v2/GTA/Global.cs
@@ -32,4 +32,30 @@ namespace GTA
 		public unsafe void SetString(string value)
 		{
 			int size = Encoding.UTF8.GetByteCount(value);
-			Marshal.Copy(Encoding.UTF8.GetBytes(value), 0, address, size
+			Marshal.Copy(Encoding.UTF8.GetBytes(value), 0, address, size);
+			*((byte*)MemoryAddress + size) = 0;
+		}
+		public unsafe void SetVector3(Vector3 value)
+		{
+			SHVDN.NativeMemory.WriteVector3(address, value.ToArray());
+		}
+
+		public unsafe int GetInt()
+		{
+			return SHVDN.NativeMemory.ReadInt32(address);
+		}
+		public unsafe float GetFloat()
+		{
+			return SHVDN.NativeMemory.ReadFloat(address);
+		}
+		public unsafe string GetString()
+		{
+			return SHVDN.NativeMemory.PtrToStringUTF8(address);
+		}
+		public unsafe Vector3 GetVector3()
+		{
+			var data = SHVDN.NativeMemory.ReadVector3(address);
+			return new Vector3(data[0], data[1], data[2]);
+		}
+	}
+}
