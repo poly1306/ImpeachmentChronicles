3eeb085ae0453d126c7814e7319b64e6b6541078
poly1306 <118820697+poly1306@users.noreply.github.com>
2020-10-25 15:55:30 +0000

    Update c2_panel.txt

--- source/scripting_v2/GTA/MessageBox.cs
+++ source/scripting_v2/GTA/MessageBox.cs
@@ -21,4 +21,104 @@ namespace GTA
 
 		public MessageBox(string headerCaption)
 		{
-			HeaderTextColor = C
+			HeaderTextColor = Color.White;
+			HeaderFont = Font.HouseScript;
+			HeaderTextScale = 0.5f;
+			HeaderCentered = true;
+			SelectedItemColor = Color.FromArgb(200, 255, 105, 180);
+			UnselectedItemColor = Color.FromArgb(200, 176, 196, 222);
+			SelectedTextColor = Color.Black;
+			UnselectedTextColor = Color.DarkSlateGray;
+			ItemFont = Font.ChaletLondon;
+			ItemTextScale = 0.4f;
+			ItemTextCentered = true;
+			Caption = headerCaption;
+
+			Width = 200;
+			Height = 50;
+			ButtonHeight = 30;
+			OkCancel = false;
+		}
+
+		public override void Draw()
+		{
+			Draw(default);
+		}
+		public override void Draw(Size offset)
+		{
+			rectBody.Draw(offset);
+			text.Draw(offset);
+			rectYes.Draw(offset);
+			rectNo.Draw(offset);
+			textYes.Draw(offset);
+			textNo.Draw(offset);
+		}
+
+		public override void Initialize()
+		{
+			rectNo = new UIRectangle(
+				new Point(Width / 2, Height),
+				new Size(Width / 2, ButtonHeight),
+				UnselectedItemColor);
+			rectYes = new UIRectangle(
+				new Point(0, Height),
+				new Size(Width / 2, ButtonHeight),
+				UnselectedItemColor);
+			rectBody = new UIRectangle(
+				default,
+				new Size(Width, Height), HeaderColor);
+			text = new UIText(
+				Caption,
+				HeaderCentered ? new Point(Width / 2, 0) : default,
+				HeaderTextScale,
+				HeaderTextColor,
+				HeaderFont,
+				HeaderCentered);
+			textNo = new UIText(
+				OkCancel ? "Cancel" : "No",
+				new Point(Width / 4 * 3, Height),
+				ItemTextScale,
+				UnselectedTextColor,
+				ItemFont,
+				ItemTextCentered);
+			textYes = new UIText(
+				OkCancel ? "OK" : "Yes",
+				new Point(Width / 4, Height),
+				ItemTextScale,
+				UnselectedTextColor,
+				ItemFont,
+				ItemTextCentered);
+
+			OnChangeItem(false);
+		}
+
+		public override void OnOpen()
+		{
+		}
+		public override void OnClose()
+		{
+		}
+		public override void OnActivate()
+		{
+			if (!selection)
+			{
+				No(this, EventArgs.Empty);
+			}
+			else
+			{
+				Yes(this, EventArgs.Empty);
+			}
+
+			Parent.PopMenu();
+		}
+
+		public override void OnChangeItem(bool right)
+		{
+			selection = !selection;
+
+			if (selection)
+			{
+				rectNo.Color = UnselectedItemColor;
+				rectYes.Color = SelectedItemColor;
+				textNo.Color = UnselectedTextColor;
+				textYes.Col
