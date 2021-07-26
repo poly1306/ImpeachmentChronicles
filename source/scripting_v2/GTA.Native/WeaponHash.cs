0ec5da47583ca67e87e91bb51991ababde2f40fb
poly1306 <118820697+poly1306@users.noreply.github.com>
2021-07-26 16:47:21 +0000

    pod spec update

---
+++ source/scripting_v2/GTA.Native/WeaponHash.cs
@@ -0,0 +1,34 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+namespace GTA.Native
+{
+	public enum WeaponHash : uint
+	{
+		Knife = 0x99B507EA,
+		Nightstick = 0x678B81B1,
+		Hammer = 0x4E875F73,
+		Bat = 0x958A4A8F,
+		GolfClub = 0x440E4788,
+		Crowbar = 0x84BD7BFD,
+		Bottle = 0xF9E6AA4B,
+		SwitchBlade = 0xDFE37640,
+		BattleAxe = 0xCD274149,
+		PoolCue = 0x94117305,
+		Wrench = 0x19044EE0,
+		StoneHatchet = 0x3813FC08,
+		CandyCane = 0x6589186A,
+		Pistol = 0x1B06D571,
+		PistolMk2 = 0xBFE256D4,
+		CombatPistol = 0x5EF9FEC4,
+		APPistol = 0x22D8FE39,
+		Pistol50 = 0x99AEEB3B,
+		FlareGun = 0x47757124,
+		MarksmanPistol = 0xDC4DB296,
+		Revolver = 0xC1B3C3D1,
+		RevolverMk2 = 0xCB96392F,
+		DoubleActionRevolver = 0x97EA20B8,
+		UpNAtomizer = 0xAF3696A1,
+		CeramicP
