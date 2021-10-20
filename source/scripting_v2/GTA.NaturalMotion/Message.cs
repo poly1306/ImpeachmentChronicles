50f561254def4b1f8da3f81d0da82d39049462ba
poly1306 <118820697+poly1306@users.noreply.github.com>
2021-10-20 12:32:55 +0000

    Make the commit graph burn :fire:

--- source/scripting_v2/GTA.NaturalMotion/Message.cs
+++ source/scripting_v2/GTA.NaturalMotion/Message.cs
@@ -60,4 +60,69 @@ namespace GTA.NaturalMotion
 				{
 					// Does not call when a CTaskNMControl task is active or the CEvent (which usually causes some task) related to CTaskNMControl occured for calling SET_PED_TO_RAGDOLL for legacy script compatibility.
 					// Otherwise, the ragdoll duration will be overridden.
-					Function.Call(Hash.SET_PED_TO_RAGDOLL, target.Handle, 10000, -1, 1, 1, 1,
+					Function.Call(Hash.SET_PED_TO_RAGDOLL, target.Handle, 10000, -1, 1, 1, 1, 0);
+				}
+			}
+
+			SetArgument("start", true);
+			SHVDN.NativeMemory.SendNmMessage(target.Handle, _message, _boolIntFloatArguments, _stringVector3ArrayArguments);
+		}
+		/// <summary>
+		///	Starts this behavior on the given <see cref="Ped"/> for a specified duration.
+		/// </summary>
+		/// <param name="target">The <see cref="Ped"/> to start the behavior to.</param>
+		/// <param name="duration">How long to apply the behavior for (-1 for looped).</param>
+		public void SendTo(Ped target, int duration)
+		{
+			if (!target.CanRagdoll)
+			{
+				target.CanRagdoll = true;
+			}
+
+			// Always call to specify the new duration
+			Function.Call(Hash.SET_PED_TO_RAGDOLL, target.Handle, 10000, duration, 1, 1, 1, 0);
+			SendTo(target);
+		}
+
+		/// <summary>
+		/// Sets an argument to a <see cref="bool"/> value.
+		/// </summary>
+		/// <param name="message">The argument name.</param>
+		/// <param name="value">The value to set the argument to.</param>
+		public void SetArgument(string message, bool value)
+		{
+			CreateBoolIntFloatArgDictIfNotCreated();
+
+			int valueConverted = value ? 1 : 0;
+			_boolIntFloatArguments[message] = (valueConverted, typeof(bool));
+		}
+		/// <summary>
+		/// Sets an argument to a <see cref="int"/> value.
+		/// </summary>
+		/// <param name="message">The argument name.</param>
+		/// <param name="value">The value to set the argument to.</param>
+		public void SetArgument(string message, int value)
+		{
+			CreateBoolIntFloatArgDictIfNotCreated();
+
+			_boolIntFloatArguments[message] = (value, typeof(int));
+		}
+		/// <summary>
+		/// Sets an argument to a <see cref="float"/> value.
+		/// </summary>
+		/// <param name="message">The argument name.</param>
+		/// <param name="value">The value to set the argument to.</param>
+		public void SetArgument(string message, float value)
+		{
+			CreateBoolIntFloatArgDictIfNotCreated();
+
+			unsafe
+			{
+				int valueConverted = *(int*)&value;
+				_boolIntFloatArguments[message] = (valueConverted, typeof(float));
+			}
+		}
+		/// <summary>
+		/// Sets an argument to a <see cref="string"/> value.
+		/// </summary>
+		/// <param name="message">The argument name.</para
