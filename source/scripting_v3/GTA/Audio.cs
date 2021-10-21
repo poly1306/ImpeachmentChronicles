1494034cf99ee1b4e78e6e9ca0ee1734577daa5a
poly1306 <118820697+poly1306@users.noreply.github.com>
2021-10-21 15:21:54 +0000

    Changed folder name

--- source/scripting_v3/GTA/Audio.cs
+++ source/scripting_v3/GTA/Audio.cs
@@ -100,3 +100,52 @@ namespace GTA
 		/// <summary>
 		/// Plays a sound from the game's sound files without transformation.
 		/// </summary>
+		/// <param name="soundFile">The sound file to play.</param>
+		/// <param name="soundSet">The name of the sound inside the file.</param>
+		/// <returns>The identifier of the active sound effect instance.</returns>
+		public static int PlaySoundFrontend(string soundFile, string soundSet)
+		{
+			var id = Function.Call<int>(Hash.GET_SOUND_ID);
+			Function.Call(Hash.PLAY_SOUND_FRONTEND, id, soundFile, soundSet, 0);
+			return id;
+		}
+
+		/// <summary>
+		/// Cancels playing the specified sound instance.
+		/// </summary>
+		/// <param name="id">The identifier of the active sound effect instance.</param>
+		public static void StopSound(int id)
+		{
+			Function.Call(Hash.STOP_SOUND, id);
+		}
+		/// <summary>
+		/// Releases the specified sound instance. Call this for every sound effect started.
+		/// </summary>
+		/// <param name="id">The identifier of the active sound effect instance.</param>
+		public static void ReleaseSound(int id)
+		{
+			Function.Call(Hash.RELEASE_SOUND_ID, id);
+		}
+
+		/// <summary>
+		/// Gets a boolean indicating whether the specified sound instance has completed playing.
+		/// </summary>
+		/// <param name="id">The identifier of the active sound effect instance.</param>
+		public static bool HasSoundFinished(int id)
+		{
+			return Function.Call<bool>(Hash.HAS_SOUND_FINISHED, id);
+		}
+
+		/// <summary>
+		/// Sets an audio flag to modify subsequent sounds.
+		/// </summary>
+		/// <param name="flag"></param>
+		/// <param name="toggle"></param>
+		public static void SetAudioFlag(AudioFlags flag, bool toggle)
+		{
+			Function.Call(Hash.SET_AUDIO_FLAG, flag.ToString(), toggle);
+		}
+
+		#endregion
+	}
+}
---
+++ source/scripting_v3/GTA/AudioFlags.cs
@@ -0,0 +1,24 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+namespace GTA
+{
+	/// <summary>
+	/// An enumeration of all possible audio flags.
+	/// </summary>
+	public enum AudioFlags
+	{
+		ActivateSwitchWheelAudio,
+		AllowCutsceneOverScreenFade,
+		AllowForceRadioAfterRetune,
+		AllowPainAndAmbientSpeechToPlayDuringCutscene,
+		AllowPlayerAIOnMission,
+		AllowPoliceScannerWhenPlayerHasNoControl,
+		AllowRadioDuringSwitch,
+		AllowRadioOverScreenFade,
+		AllowScoreAndRadio,
+		AllowScriptedSpeechInSlowMo,
+		AvoidMissionCompleteDelay,
+		DisableAbortConve
