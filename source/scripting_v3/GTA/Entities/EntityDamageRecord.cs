f9c4c2d21760a7c461d808f40fa246dd130c7ea4
poly1306 <118820697+poly1306@users.noreply.github.com>
2021-12-04 20:04:12 +0000

    Removes unused imports

--- source/scripting_v3/GTA/Entities/EntityDamageRecord.cs
+++ source/scripting_v3/GTA/Entities/EntityDamageRecord.cs
@@ -63,4 +63,45 @@ namespace GTA
 		}
 
 		/// <summary>
-		/// Determines if <paramref name="entityD
+		/// Determines if <paramref name="entityDamageRecord"/> has the same properties as this <see cref="EntityDamageRecord"/>.
+		/// </summary>
+		/// <param name="entityDamageRecord">The <see cref="object"/> to check.</param>
+		/// <returns><c>true</c> if the <paramref name="entityDamageRecord"/> has the same properties as this <see cref="EntityDamageRecord"/>; otherwise, <c>false</c>.</returns>
+		public bool Equals(EntityDamageRecord entityDamageRecord)
+		{
+			return Victim == entityDamageRecord.Victim &&
+				Attacker == entityDamageRecord.Attacker &&
+				WeaponHash == entityDamageRecord.WeaponHash &&
+				GameTime == entityDamageRecord.GameTime;
+		}
+		/// <summary>
+		/// Determines if an <see cref="object"/> is an <see cref="EntityDamageRecord"/> and has the same properties as this <see cref="EntityDamageRecord"/>.
+		/// </summary>
+		/// <param name="obj">The <see cref="object"/> to check.</param>
+		/// <returns><c>true</c> if the <paramref name="obj"/> is an <see cref="EntityDamageRecord"/> and has the same properties as this <see cref="EntityDamageRecord"/>; otherwise, <c>false</c>.</returns>
+		public override bool Equals(object obj)
+		{
+			if (obj is EntityDamageRecord entityDamageRecord)
+			{
+				Equals(entityDamageRecord);
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Determines if two <see cref="EntityDamageRecord"/>s have the same properties.
+		/// </summary>
+		/// <param name="left">The left <see cref="Entity"/>.</param>
+		/// <param name="right">The right <see cref="Entity"/>.</param>
+		/// <returns><c>true</c> if <paramref name="left"/> has the same properties as <paramref name="right"/>; otherwise, <c>false</c>.</returns>
+		public static bool operator ==(EntityDamageRecord left, EntityDamageRecord right)
+		{
+			return left.Equals(right);
+		}
+		/// <summary>
+		/// Determines if two <see cref="Entity"/>s do not have the same properties.
+		/// </summary>
+		/// <param name="left">The left <see cref="Entity"/>.</param>
+		/// <param name="right">The right <see cref="Entity"/>.</param>
+		/// <returns><c>true</c> if <paramref name="left"/> does not have the same properties as <paramref name="right"/>; otherwise, <c>false<
