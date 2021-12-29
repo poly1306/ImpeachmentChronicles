5a1dfef206af56bfe2e020e37ccbf0006e213ac5
poly1306 <118820697+poly1306@users.noreply.github.com>
2021-12-29 12:11:54 +0000

    update configs

--- source/scripting_v3/GTA/Entities/Peds/Ped.cs
+++ source/scripting_v3/GTA/Entities/Peds/Ped.cs
@@ -1561,4 +1561,31 @@ namespace GTA
 		/// Applies a relative force to this <see cref="Entity"/> using relative offset.
 		/// </summary>
 		/// <inheritdoc cref="ApplyForceInternal(Vector3, Vector3, ForceType, RagdollComponent, bool, bool, bool, bool, bool)"/>
-		public void ApplyRelativeForceRelativeOffset(Vector
+		public void ApplyRelativeForceRelativeOffset(Vector3 force, Vector3 offset, ForceType forceType, RagdollComponent component, bool scaleByMass, bool triggerAudio = false, bool scaleByTimeScale = true)
+		{
+			ApplyForceInternal(force, offset, forceType, component, true, true, scaleByMass, triggerAudio, scaleByTimeScale);
+		}
+		/// <summary>
+		/// Applies a force to this <see cref="Entity"/>.
+		/// </summary>
+		/// <param name="force">The force to be applied.</param>
+		/// <param name="offset">The offset from center of entity at which to apply force.</param>
+		/// <param name="forceType">Type of the force to apply.</param>
+		/// <param name="component">Component of the entity to apply the force.</param>
+		/// <param name="relativeForce">
+		/// Specifies whether the force vector passed in is in relative or world coordinates.
+		/// Relative coordinates (<see langword="true"/>) means the force will get automatically transformed into world space before being applied.
+		/// </param>
+		/// <param name="relativeOffset">Specifies whether the offset passed in is in relative or world coordinates.</param>
+		/// <param name="scaleByMass">
+		/// <para>Specifies whether to scale the force by mass.</para>
+		/// <para>If <see langword="true"/>, force will be multiplied by mass. For example, force passed in is in fact an acceleration rate in <c>m/s*s</c> (force) or velocity change in <c>m/s</c> (impulse).</para>
+		/// <para>If <see langword="false"/>, force will be applied directly and it's effect will depend on the mass of the entity. For example, force passed in is a proper force in Newtons (force) or a step change in momentum <c>kg*m/s</c> (impulse).</para>
+		/// <para>
+		/// In other words, scaling by mass is probably easier in most situations -
+		/// if the mass of the object changes it's behaviour shouldn't, and it's easier to picture the effect because an acceleration rate of <c>10.0</c> is approximately the same as gravity (<c>9.81</c> to be more precise).
+		/// </para>
+		/// </param>
+		/// <param name="triggerAudio">
+		/// <para>Specifies whether to play audio events related to the force being applied. The sound will play only if the entity type is <see cref="Vehicle"/> and will play a suspension squeal depending on the magnitude of the force.</para>
+		/// <para
