c2c03ec5489e28019479a6377f1e2385eab5382d
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-01-03 16:17:26 +0000

    camelCase

--- source/scripting_v3/GTA/Entities/Peds/PedMoveBlendRatio.cs
+++ source/scripting_v3/GTA/Entities/Peds/PedMoveBlendRatio.cs
@@ -33,4 +33,59 @@ namespace GTA
 		/// <summary>
 		/// Returns the same struct as <c>new PedMoveBlendRatio(1.0f)</c>.
 		/// </summary>
-		public static PedMoveBlendRatio Walk => ne
+		public static PedMoveBlendRatio Walk => new PedMoveBlendRatio(1.0f);
+		/// <summary>
+		/// Returns the same struct as <c>new PedMoveBlendRatio(2.0f)</c>.
+		/// </summary>
+		public static PedMoveBlendRatio Run => new PedMoveBlendRatio(2.0f);
+		/// <summary>
+		/// Returns the same struct as <c>new PedMoveBlendRatio(3.0f)</c>.
+		/// </summary>
+		public static PedMoveBlendRatio Sprint => new PedMoveBlendRatio(3.0f);
+
+		public static implicit operator PedMoveBlendRatio(float value) => new PedMoveBlendRatio(value);
+		public static explicit operator float(PedMoveBlendRatio value) => value.Value;
+		public static implicit operator InputArgument(PedMoveBlendRatio value)
+		{
+			return new InputArgument(value.Value);
+		}
+
+		/// <summary>
+		/// Tests for equality between two objects.
+		/// </summary>
+		/// <param name="left">The first value to compare.</param>
+		/// <param name="right">The second value to compare.</param>
+		/// <returns><see langword="true" /> if <paramref name="left"/> has the same value as <paramref name="right"/>; otherwise, <see langword="false" />.</returns>
+		public static bool operator ==(PedMoveBlendRatio left, PedMoveBlendRatio right) => Equals(left, right);
+		/// <summary>
+		/// Tests for inequality between two objects.
+		/// </summary>
+		/// <param name="left">The first value to compare.</param>
+		/// <param name="right">The second value to compare.</param>
+		/// <returns><see langword="true" /> if <paramref name="left"/> has a different value than <paramref name="right"/>; otherwise, <see langword="false" />.</returns>
+		public static bool operator !=(PedMoveBlendRatio left, PedMoveBlendRatio right) => !Equals(left, right);
+
+		public bool Equals(PedMoveBlendRatio moveBlendRatio)
+		{
+			return Value == moveBlendRatio.Value;
+		}
+		public override bool Equals(object obj)
+		{
+			if (obj is PedMoveBlendRatio asset)
+			{
+				return Equals(asset);
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Returns the hash code for this instance.
+		/// </summary>
+		/// <returns>A 32-bit signed integer hash code.</returns>
+		public override int GetHashCode() => Value.GetHashCode();
+
+		/// <summary>
+		/// Converts the value of the object to its equivalent string representation.
+		/// </summary>
+		/// <returns>The string representation of the value of this in
