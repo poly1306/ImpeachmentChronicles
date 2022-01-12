98d69d9ceadfdd305196d6428df27862e9b409db
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-01-12 12:26:37 +0000

    chore(package): update eslint to version 3.8.0

---
+++ source/scripting_v3/GTA/Entities/Peds/SpeechModifier.cs
@@ -0,0 +1,16 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+namespace GTA
+{
+	public enum SpeechModifier
+	{
+		Standard,
+		AllowRepeat,
+		Beat,
+		Force,
+		ForceFrontend,
+		ForceNoRepeatFrontend,
+		ForceNor
