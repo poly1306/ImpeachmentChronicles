360df159fa6d2aabe51786fa13418d5148785b21
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-02-11 21:24:53 +0000

    Add travis badge.

--- source/scripting_v3/GTA/Player.cs
+++ source/scripting_v3/GTA/Player.cs
@@ -162,4 +162,71 @@ namespace GTA
 			set => Function.Call(Hash.SET_PLAYER_PARACHUTE_TINT_INDEX, Handle, value);
 		}
 		/// <summary>
-		/// Gets or sets the reserv
+		/// Gets or sets the reserve parachute tint for this <see cref="Player"/>.
+		/// </summary>
+		public ParachuteTint ReserveParachuteTint
+		{
+			get
+			{
+				int result;
+				unsafe
+				{
+					Function.Call(Hash.GET_PLAYER_RESERVE_PARACHUTE_TINT_INDEX, Handle, &result);
+				}
+				return (ParachuteTint)result;
+			}
+			set => Function.Call(Hash.SET_PLAYER_RESERVE_PARACHUTE_TINT_INDEX, Handle, value);
+		}
+
+		/// <summary>
+		/// Sets a value indicating whether this <see cref="Player"/> can leave a parachute smoke trail.
+		/// </summary>
+		/// <value>
+		/// <see langword="true" /> if this <see cref="Player"/> can leave a parachute smoke trail; otherwise, <see langword="false" />.
+		/// </value>
+		public bool CanLeaveParachuteSmokeTrail
+		{
+			set => Function.Call(Hash.SET_PLAYER_CAN_LEAVE_PARACHUTE_SMOKE_TRAIL, Handle, value);
+		}
+
+		/// <summary>
+		/// Gets or sets the color of the parachute smoke trail for this <see cref="Player"/>.
+		/// </summary>
+		/// <value>
+		/// The color of the parachute smoke trail for this <see cref="Player"/>.
+		/// </value>
+		public Color ParachuteSmokeTrailColor
+		{
+			get
+			{
+				int r, g, b;
+				unsafe
+				{
+					Function.Call(Hash.GET_PLAYER_PARACHUTE_SMOKE_TRAIL_COLOR, Handle, &r, &g, &b);
+				}
+				return Color.FromArgb(r, g, b);
+			}
+			set => Function.Call(Hash.SET_PLAYER_PARACHUTE_SMOKE_TRAIL_COLOR, Handle, value.R, value.G, value.B);
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="Player"/> is dead.
+		/// </summary>
+		/// <value>
+		///   <see langword="true" /> if this <see cref="Player"/> is dead; otherwise, <see langword="false" />.
+		/// </value>
+		public bool IsDead => Function.Call<bool>(Hash.IS_PLAYER_DEAD, Handle);
+
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="Player"/> is alive.
+		/// </summary>
+		/// <value>
+		///   <see langword="true" /> if this <see cref="Player"/> is alive; otherwise, <see langword="false" />.
+		/// </value>
+		public bool IsAlive => !IsDead;
+
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="Player"/> is aiming.
+		/// </summary>
+		/// <value>
+		///   <see langword="true" /> if this <see cref="Player"/> is aiming; otherwise, <see lan
