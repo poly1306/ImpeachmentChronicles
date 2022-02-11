fc40c79797af15cc51dd3fbecf4f3e851bcefd83
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-02-11 06:58:19 +0000

    Delete glyphicons-halflings-regular.woff

--- source/scripting_v3/GTA/Player.cs
+++ source/scripting_v3/GTA/Player.cs
@@ -73,4 +73,93 @@ namespace GTA
 					case PedHash.Franklin:
 						stat = Game.GenerateHash("SP1_TOTAL_CASH");
 						break;
-					case PedHash
+					case PedHash.Trevor:
+						stat = Game.GenerateHash("SP2_TOTAL_CASH");
+						break;
+					default:
+						return 0;
+				}
+
+				int result;
+				unsafe
+				{
+					Function.Call(Hash.STAT_GET_INT, stat, &result, -1);
+				}
+
+				return result;
+			}
+			set
+			{
+				int stat;
+
+				switch ((PedHash)Character.Model.Hash)
+				{
+					case PedHash.Michael:
+						stat = Game.GenerateHash("SP0_TOTAL_CASH");
+						break;
+					case PedHash.Franklin:
+						stat = Game.GenerateHash("SP1_TOTAL_CASH");
+						break;
+					case PedHash.Trevor:
+						stat = Game.GenerateHash("SP2_TOTAL_CASH");
+						break;
+					default:
+						return;
+				}
+
+				Function.Call(Hash.STAT_SET_INT, stat, value, 1);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the wanted level for this <see cref="Player"/>.
+		/// </summary>
+		public int WantedLevel
+		{
+			get => Function.Call<int>(Hash.GET_PLAYER_WANTED_LEVEL, Handle);
+			set
+			{
+				Function.Call(Hash.SET_PLAYER_WANTED_LEVEL, Handle, value, false);
+				Function.Call(Hash.SET_PLAYER_WANTED_LEVEL_NOW, Handle, false);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the wanted center position for this <see cref="Player"/>.
+		/// </summary>
+		/// <value>
+		/// The place in world coordinates where the police think this <see cref="Player"/> is.
+		/// </value>
+		public Vector3 WantedCenterPosition
+		{
+			get => Function.Call<Vector3>(Hash.GET_PLAYER_WANTED_CENTRE_POSITION, Handle);
+			set => Function.Call(Hash.SET_PLAYER_WANTED_CENTRE_POSITION, Handle, value.X, value.Y, value.Z);
+		}
+
+		/// <summary>
+		/// Gets or sets the maximum amount of armor this <see cref="Player"/> can carry.
+		/// </summary>
+		public int MaxArmor
+		{
+			get => Function.Call<int>(Hash.GET_PLAYER_MAX_ARMOUR, Handle);
+			set => Function.Call(Hash.SET_PLAYER_MAX_ARMOUR, Handle, value);
+		}
+
+		/// <summary>
+		/// Gets or sets the primary parachute tint for this <see cref="Player"/>.
+		/// </summary>
+		public ParachuteTint PrimaryParachuteTint
+		{
+			get
+			{
+				int result;
+				unsafe
+				{
+					Function.Call(Hash.GET_PLAYER_PARACHUTE_TINT_INDEX, Handle, &result);
+				}
+				return (ParachuteTint)result;
+			}
+			set => Function.Call(Hash.SET_PLAYER_PARACHUTE_TINT_INDEX, Handle, value);
+		}
+		/// <summary>
+		/// Gets or sets the reserv
