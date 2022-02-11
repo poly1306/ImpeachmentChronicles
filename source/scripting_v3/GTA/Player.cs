b7024eeb084d1a7ab70ebc6f1272006228a6c8f8
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-02-11 05:45:49 +0000

    Idk

---
+++ source/scripting_v3/GTA/Player.cs
@@ -0,0 +1,76 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+using GTA.Math;
+using GTA.Native;
+using System.Drawing;
+
+namespace GTA
+{
+	public sealed class Player : INativeValue
+	{
+		#region Fields
+		Ped ped;
+		#endregion
+
+		internal Player(int handle)
+		{
+			Handle = handle;
+		}
+
+		public int Handle
+		{
+			get;
+			private set;
+		}
+
+		public ulong NativeValue
+		{
+			get => (ulong)Handle;
+			set => Handle = unchecked((int)value);
+		}
+
+		/// <summary>
+		/// Gets the <see cref="Ped"/> this <see cref="Player"/> is controlling.
+		/// </summary>
+		public Ped Character
+		{
+			get
+			{
+				int handle = Function.Call<int>(Hash.GET_PLAYER_PED, Handle);
+
+				if (ped == null || handle != ped.Handle)
+				{
+					ped = new Ped(handle);
+				}
+
+				return ped;
+			}
+		}
+
+		/// <summary>
+		/// Gets the Social Club name of this <see cref="Player"/>.
+		/// </summary>
+		public string Name => Function.Call<string>(Hash.GET_PLAYER_NAME, Handle);
+
+		/// <summary>
+		/// Gets or sets how much money this <see cref="Player"/> has.
+		/// <remarks>Only works if current player is <see cref="PedHash.Michael"/>, <see cref="PedHash.Franklin"/> or <see cref="PedHash.Trevor"/></remarks>
+		/// </summary>
+		public int Money
+		{
+			get
+			{
+				int stat;
+
+				switch ((PedHash)Character.Model.Hash)
+				{
+					case PedHash.Michael:
+						stat = Game.GenerateHash("SP0_TOTAL_CASH");
+						break;
+					case PedHash.Franklin:
+						stat = Game.GenerateHash("SP1_TOTAL_CASH");
+						break;
+					case PedHash
