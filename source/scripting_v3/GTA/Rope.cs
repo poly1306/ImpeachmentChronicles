1841a54761b24419c3de07cb08e29ad0a0f99137
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-02-27 07:36:37 +0000

    Added a favicon

---
+++ source/scripting_v3/GTA/Rope.cs
@@ -0,0 +1,11 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+using GTA.Math;
+using GTA.Native;
+
+namespace GTA
+{
+	public sealed clas
