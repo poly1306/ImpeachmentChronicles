c934899cc9ba663a0a41e5b96473bb20084640fa
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-03-01 20:32:46 +0000

    Adding journal entries from today

--- source/scripting_v3/GTA/Scaleform.cs
+++ source/scripting_v3/GTA/Scaleform.cs
@@ -83,4 +83,60 @@ namespace GTA
 				}
 				else if (argument is double argDouble)
 				{
-					Function.Call(Hash.SCALEFORM_MOVIE_METHOD_ADD_PARAM_FLOAT, (float)arg
+					Function.Call(Hash.SCALEFORM_MOVIE_METHOD_ADD_PARAM_FLOAT, (float)argDouble);
+				}
+				else if (argument is bool argBool)
+				{
+					Function.Call(Hash.SCALEFORM_MOVIE_METHOD_ADD_PARAM_BOOL, (bool)argBool);
+				}
+				else if (argument is ScaleformArgumentTXD argTxd)
+				{
+					Function.Call(Hash.SCALEFORM_MOVIE_METHOD_ADD_PARAM_TEXTURE_NAME_STRING, argTxd.txd);
+				}
+				else
+				{
+					throw new ArgumentException(string.Format("Unknown argument type {0} passed to scaleform with handle {1}.", argument.GetType().Name, Handle), "arguments");
+				}
+			}
+		}
+
+		public void CallFunction(string function, params object[] arguments)
+		{
+			CallFunctionHead(function, arguments);
+			Function.Call(Hash.END_SCALEFORM_MOVIE_METHOD);
+		}
+		public int CallFunctionReturn(string function, params object[] arguments)
+		{
+			CallFunctionHead(function, arguments);
+			return Function.Call<int>(Hash.END_SCALEFORM_MOVIE_METHOD_RETURN_VALUE);
+		}
+
+		public void Render2D()
+		{
+			Function.Call(Hash.DRAW_SCALEFORM_MOVIE_FULLSCREEN, Handle, 255, 255, 255, 255, 0);
+		}
+		public void Render2DScreenSpace(PointF position, PointF size)
+		{
+			float x = position.X / UI.Screen.Width;
+			float y = position.Y / UI.Screen.Height;
+			float w = size.X / UI.Screen.Width;
+			float h = size.Y / UI.Screen.Height;
+
+			Function.Call(Hash.DRAW_SCALEFORM_MOVIE, Handle, x + (w * 0.5f), y + (h * 0.5f), w, h, 255, 255, 255, 255);
+		}
+
+		public void Render3D(Vector3 position, Vector3 rotation, Vector3 scale)
+		{
+			Function.Call(Hash.DRAW_SCALEFORM_MOVIE_3D_SOLID, Handle, position.X, position.Y, position.Z, rotation.X, rotation.Y, rotation.Z, 2.0f, 2.0f, 1.0f, scale.X, scale.Y, scale.Z, 2);
+		}
+		public void Render3DAdditive(Vector3 position, Vector3 rotation, Vector3 scale)
+		{
+			Function.Call(Hash.DRAW_SCALEFORM_MOVIE_3D, Handle, position.X, position.Y, position.Z, rotation.X, rotation.Y, rotation.Z, 2.0f, 2.0f, 1.0f, scale.X, scale.Y, scale.Z, 2);
+		}
+
+		public static implicit operator InputArgument(Scaleform value)
+		{
+			return new InputArgument((ulong)value.Handle);
+		}
+	}
+}
---
+++ source/scripting_v3/GTA/ScaleformArgumentTXD.cs
@@ -0,0 +1,10 @@
+//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+namespace GTA
+{
+	public sealed class ScaleformArgumentTXD
+	{
+		internal strin
