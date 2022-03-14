e7652404718ab886f255e616e376c044dc3a278b
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-03-14 10:34:31 +0000

    Update LICENSE year

--- source/scripting_v3/GTA/Weapons/Weapon.cs
+++ source/scripting_v3/GTA/Weapons/Weapon.cs
@@ -4,4 +4,113 @@
 //
 
 using GTA.Native;
-usi
+using System.Linq;
+
+namespace GTA
+{
+	public sealed class Weapon
+	{
+		#region Fields
+		readonly Ped owner;
+		WeaponComponentCollection components;
+		#endregion
+
+		internal Weapon()
+		{
+			Hash = WeaponHash.Unarmed;
+		}
+		internal Weapon(Ped owner, WeaponHash hash)
+		{
+			this.owner = owner;
+			Hash = hash;
+		}
+
+		public WeaponHash Hash
+		{
+			get;
+		}
+
+		public string DisplayName => GetDisplayNameFromHash(Hash);
+
+		public string LocalizedName => Game.GetLocalizedString((int)SHVDN.NativeMemory.GetHumanNameHashOfWeaponInfo((uint)Hash));
+
+		public bool IsPresent => Hash == WeaponHash.Unarmed || Function.Call<bool>(Native.Hash.HAS_PED_GOT_WEAPON, owner.Handle, Hash);
+
+		public Model Model => new Model(Function.Call<int>(Native.Hash.GET_WEAPONTYPE_MODEL, Hash));
+
+		public WeaponTint Tint
+		{
+			get => Function.Call<WeaponTint>(Native.Hash.GET_PED_WEAPON_TINT_INDEX, owner.Handle, Hash);
+			set => Function.Call(Native.Hash.SET_PED_WEAPON_TINT_INDEX, owner.Handle, Hash, value);
+		}
+
+		public WeaponGroup Group => Function.Call<WeaponGroup>(Native.Hash.GET_WEAPONTYPE_GROUP, Hash);
+
+		public int Ammo
+		{
+			get
+			{
+				if (Hash == WeaponHash.Unarmed)
+				{
+					return 1;
+				}
+
+				if (!IsPresent)
+				{
+					return 0;
+				}
+
+				return Function.Call<int>(Native.Hash.GET_AMMO_IN_PED_WEAPON, owner.Handle, Hash);
+			}
+			set
+			{
+				if (Hash == WeaponHash.Unarmed)
+				{
+					return;
+				}
+
+				if (IsPresent)
+				{
+					Function.Call(Native.Hash.SET_PED_AMMO, owner.Handle, Hash, value);
+				}
+				else
+				{
+					Function.Call(Native.Hash.GIVE_WEAPON_TO_PED, owner.Handle, Hash, value, false, true);
+				}
+			}
+		}
+		public int AmmoInClip
+		{
+			get
+			{
+				if (Hash == WeaponHash.Unarmed)
+				{
+					return 1;
+				}
+
+				if (!IsPresent)
+				{
+					return 0;
+				}
+
+				int ammoInClip;
+				unsafe
+				{
+					Function.Call(Native.Hash.GET_AMMO_IN_CLIP, owner.Handle, Hash, &ammoInClip);
+				}
+				return ammoInClip;
+			}
+			set
+			{
+				if (Hash == WeaponHash.Unarmed)
+				{
+					return;
+				}
+
+				if (IsPresent)
+				{
+					Function.Call(Native.Hash.SET_AMMO_IN_CLIP, owner.Handle, Hash, value);
+				}
+				else
+				{
+					Function.Call(Native.Hash.GIVE_WEAPON_TO_PED, owner.Handle, Hash, value, true, false);
