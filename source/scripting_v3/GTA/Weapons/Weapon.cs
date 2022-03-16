07854a71f80f449dc9fe9e10f1f4a5cb261106ad
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-03-16 13:40:02 +0000

    Typo in changelog

--- source/scripting_v3/GTA/Weapons/Weapon.cs
+++ source/scripting_v3/GTA/Weapons/Weapon.cs
@@ -114,3 +114,102 @@ namespace GTA
 				else
 				{
 					Function.Call(Native.Hash.GIVE_WEAPON_TO_PED, owner.Handle, Hash, value, true, false);
+				}
+			}
+		}
+
+		public int MaxAmmo
+		{
+			get
+			{
+				if (Hash == WeaponHash.Unarmed)
+				{
+					return 1;
+				}
+
+				int maxAmmo;
+				unsafe
+				{
+					Function.Call(Native.Hash.GET_MAX_AMMO, owner.Handle, Hash, &maxAmmo);
+				}
+				return maxAmmo;
+			}
+		}
+		public int MaxAmmoInClip
+		{
+			get
+			{
+				if (Hash == WeaponHash.Unarmed)
+				{
+					return 1;
+				}
+
+				if (!IsPresent)
+				{
+					return 0;
+				}
+
+				return Function.Call<int>(Native.Hash.GET_MAX_AMMO_IN_CLIP, owner.Handle, Hash, true);
+			}
+		}
+
+		public int DefaultClipSize => Function.Call<int>(Native.Hash.GET_WEAPON_CLIP_SIZE, Hash);
+
+		public bool InfiniteAmmo
+		{
+			set
+			{
+				if (Hash == WeaponHash.Unarmed)
+				{
+					return;
+				}
+
+				Function.Call(Native.Hash.SET_PED_INFINITE_AMMO, owner.Handle, value, Hash);
+			}
+		}
+		public bool InfiniteAmmoClip
+		{
+			set => Function.Call(Native.Hash.SET_PED_INFINITE_AMMO_CLIP, owner.Handle, value);
+		}
+
+		public bool CanUseOnParachute => Function.Call<bool>(Native.Hash.CAN_USE_WEAPON_ON_PARACHUTE, Hash);
+
+		public WeaponComponentCollection Components => components ?? (components = new WeaponComponentCollection(owner, this));
+
+		public static implicit operator WeaponHash(Weapon weapon)
+		{
+			return weapon.Hash;
+		}
+
+		public static string GetDisplayNameFromHash(WeaponHash hash)
+		{
+			// Will be found in this switch statement if the hash is one of the weapon hashes for singleplayer
+			switch (hash)
+			{
+				case WeaponHash.Unarmed:
+					return "WT_UNARMED";
+				case WeaponHash.Knife:
+					return "WT_KNIFE";
+				case WeaponHash.Nightstick:
+					return "WT_NGTSTK";
+				case WeaponHash.Hammer:
+					return "WT_HAMMER";
+				case WeaponHash.Bat:
+					return "WT_BAT";
+				case WeaponHash.Crowbar:
+					return "WT_CROWBAR";
+				case WeaponHash.GolfClub:
+					return "WT_GOLFCLUB";
+				case WeaponHash.Pistol:
+					return "WT_PIST";
+				case WeaponHash.CombatPistol:
+					return "WT_PIST_CBT";
+				case WeaponHash.Pistol50:
+					return "WT_PIST_50";
+				case WeaponHash.APPistol:
+					return "WT_PIST_AP";
+				case WeaponHash.StunGun:
+					return "WT_STUN";
+				case WeaponHash.MicroSMG:
+					return "WT_SMG_MCR";
+				case Weapon
