b2e8dcaebc5857c6a7e56d21fba402221e7bfe29
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-03-27 11:48:52 +0000

    Delete debugger.xml

--- source/scripting_v3/GTA/Weapons/WeaponComponentCollection.cs
+++ source/scripting_v3/GTA/Weapons/WeaponComponentCollection.cs
@@ -23,4 +23,88 @@ namespace GTA
 		{
 			_owner = owner;
 			_weapon = weapon;
-			_components = GetComponentsFromHash(wea
+			_components = GetComponentsFromHash(weapon.Hash);
+		}
+
+		public WeaponComponent this[int index]
+		{
+			get
+			{
+				WeaponComponent component = null;
+				if (index >= 0 && index < Count)
+				{
+					WeaponComponentHash componentHash = _components[index];
+
+					if (!_weaponComponents.TryGetValue(componentHash, out component))
+					{
+						component = new WeaponComponent(_owner, _weapon, componentHash);
+						_weaponComponents.Add(componentHash, component);
+					}
+					return component;
+				}
+				else
+				{
+					return _invalidComponent;
+				}
+			}
+		}
+
+		public WeaponComponent this[WeaponComponentHash componentHash]
+		{
+			get
+			{
+				if (_components.Contains(componentHash))
+				{
+					WeaponComponent component = null;
+					if (!_weaponComponents.TryGetValue(componentHash, out component))
+					{
+						component = new WeaponComponent(_owner, _weapon, componentHash);
+						_weaponComponents.Add(componentHash, component);
+					}
+
+					return component;
+				}
+				else
+				{
+					return _invalidComponent;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Gets the number of compatible components.
+		/// </summary>
+		public int Count => _components.Length;
+
+		public IEnumerator<WeaponComponent> GetEnumerator()
+		{
+			WeaponComponent[] AllComponents = new WeaponComponent[Count];
+			for (int i = 0; i < Count; i++)
+			{
+				AllComponents[i] = this[_components[i]];
+			}
+			return (AllComponents as IEnumerable<WeaponComponent>).GetEnumerator();
+		}
+
+		/// <summary>
+		/// Gets the clip component at the index.
+		/// </summary>
+		/// <param name="index">The index of the clip component subset of all the weapon component array.</param>
+		/// <returns>
+		/// A <see cref="WeaponComponent"/> instance if the <see cref="WeaponComponent"/> at the <paramref name="index"/> of the clip component subset is found;
+		/// otherwise, the <see cref="WeaponComponent"/> instance representing the invalid component.
+		/// </returns>
+		public WeaponComponent GetClipComponent(int index)
+		{
+			foreach (var component in this)
+			{
+				if (component.AttachmentPoint == WeaponAttachmentPoint.Clip ||
+					component.AttachmentPoint == WeaponAttachmentPoint.Clip2)
+				{
+					if (index-- == 0)
+					{
+						return component;
+					}
+				}
+			}
+			retu
