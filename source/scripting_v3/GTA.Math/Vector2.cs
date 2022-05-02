994a8a2dbaf146983c359ed309438a9c60737c4d
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-05-02 09:32:54 +0000

    Removed debug info.

--- source/scripting_v3/GTA.Math/Vector2.cs
+++ source/scripting_v3/GTA.Math/Vector2.cs
@@ -73,4 +73,96 @@ namespace GTA.Math
 		/// <summary>
 		/// Returns the up vector. (0,1)
 		/// </summary>
-		public static Vector2 Up => new Vector2(0
+		public static Vector2 Up => new Vector2(0.0f, 1.0f);
+
+		/// <summary>
+		/// Returns the down vector. (0,-1)
+		/// </summary>
+		public static Vector2 Down => new Vector2(0.0f, -1.0f);
+
+		/// <summary>
+		/// Returns the right vector. (1,0)
+		/// </summary>
+		public static Vector2 Right => new Vector2(1.0f, 0.0f);
+
+		/// <summary>
+		/// Returns the left vector. (-1,0)
+		/// </summary>
+		public static Vector2 Left => new Vector2(-1.0f, 0.0f);
+
+		/// <summary>
+		/// Gets or sets the component at the specified index.
+		/// </summary>
+		/// <value>The value of the X or Y component, depending on the index.</value>
+		/// <param name="index">The index of the component to access. Use 0 for the X component and 1 for the Y component.</param>
+		/// <returns>The value of the component at the specified index.</returns>
+		/// <exception cref="System.ArgumentOutOfRangeException">Thrown when the <paramref name="index"/> is out of the range [0, 1].</exception>
+		public float this[int index]
+		{
+			get
+			{
+				switch (index)
+				{
+					case 0:
+						return X;
+					case 1:
+						return Y;
+				}
+
+				throw new ArgumentOutOfRangeException("index", "Indices for Vector2 run from 0 to 1, inclusive.");
+			}
+
+			set
+			{
+				switch (index)
+				{
+					case 0:
+						X = value;
+						break;
+					case 1:
+						Y = value;
+						break;
+					default:
+						throw new ArgumentOutOfRangeException("index", "Indices for Vector2 run from 0 to 1, inclusive.");
+				}
+			}
+		}
+
+		/// <summary>
+		/// Calculates the length of the vector.
+		/// </summary>
+		/// <returns>The length of the vector.</returns>
+		public float Length()
+		{
+			return (float)System.Math.Sqrt((X * X) + (Y * Y));
+		}
+
+		/// <summary>
+		/// Calculates the squared length of the vector.
+		/// </summary>
+		/// <returns>The squared length of the vector.</returns>
+		public float LengthSquared()
+		{
+			return (X * X) + (Y * Y);
+		}
+
+		/// <summary>
+		/// Converts the vector into a unit vector.
+		/// </summary>
+		public void Normalize()
+		{
+			float length = Length();
+			if (length == 0)
+			{
+				return;
+			}
+
+			float num = 1 / length;
+			X *= num;
+			Y *= num;
+		}
+
+		/// <summary>
+		/// Calculates the distance between two vectors.
+		/// </summary>
+		/// <para
