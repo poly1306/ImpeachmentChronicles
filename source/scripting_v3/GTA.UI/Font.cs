4467529833bd705ca8dd732b56f5b3936d4addad
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-12-08 11:01:27 +0000

    jsdocs

--- source/scripting_v3/GTA.UI/Font.cs
+++ source/scripting_v3/GTA.UI/Font.cs
@@ -21,4 +21,40 @@ namespace GTA.UI
 		/// <summary>
 		/// <para>This font is for the cursive font.</para>
 		/// <para>
-		/// Sign Painter House Brush fr
+		/// Sign Painter House Brush from House Industries will be used when the game language is set to a non-CJK language.
+		/// The standard font, which is the same as <see cref="ChaletLondon"/>, will be used when the game language is set to a CJK language unless the player have custom font files installed.
+		/// </para>
+		/// </summary>
+		HouseScript,
+		/// <summary>
+		/// <para>This font contains upper ASCII characters and some other shapes such as ones for tags.</para>
+		/// <para>This font style specifies the same font unless the player have custom font files installed.</para>
+		/// </summary>
+		Monospace,
+		/// <summary>
+		/// <para>This font contains only Chevron arrows, some shield symbols, and hexagons.</para>
+		/// <para>This font style specifies the same font unless the player have custom font files installed.</para>
+		/// </summary>
+		Leaderboard,
+		/// <summary>
+		/// <para>
+		/// This font is for the condensed font for gamer tags or the distance info on the radar.
+		/// You should use <see cref="ChaletComprimeCologneNotGamerName"/> if you want to draw strings with condensed font if possible but strings can contain some CJK characters (e.g. localized strings from gxt files).
+		/// </para>
+		/// <para>
+		/// The native functions for text drawing will use the condensed font Chalet Comprim? Cologne 1960 regardness of the game language setting.
+		/// The natives will draw strings that contain only non-CJK characters without any trouble,
+		/// but without having custom font files installed, the natives will draw rectangles (a.k.a. tofus) instead of CJK characters because Chalet Comprim? Cologne 1960 doesn't contain any CJK characters.
+		/// </para>
+		/// <para>This font style specifies the same font unless the player have custom font files installed.</para>
+		/// </summary>
+		ChaletComprimeCologne,
+		/// <summary>
+		/// <para>This font contains only glyphs of numbers (0 to 9), dollar sign, asterisk, plus sign, colon, semicolon, equals sign, slash, and backslash in ASCII.</para>
+		/// <para>This font style specifies the same font unless the player have custom font files installed.</para>
+		/// </summary>
+		FixedWidthNumbersStyle,
+		/// <summary>
+		/// <para>
+		/// This font is for the condensed font for generic uses.
+		//
