e474240a7bba2fbda01e379f0bccbb89c7114e79
poly1306 <118820697+poly1306@users.noreply.github.com>
2022-12-31 07:04:57 +0000

    # code review [ci skip]

--- source/scripting_v3/GTA.UI/TextElement.cs
+++ source/scripting_v3/GTA.UI/TextElement.cs
@@ -56,4 +56,29 @@ namespace GTA.UI
 		/// <param name="scale">Sets a <see cref="Scale"/> used to increase of decrease the size of the <see cref="TextElement"/>, for no scaling use 1.0f.</param>
 		/// <param name="color">Set the <see cref="Color"/> used to draw the <see cref="TextElement"/>.</param>
 		/// <param name="font">Sets the <see cref="Font"/> used when drawing the text.</param>
-		/// <param name="alignment">Sets the <see cref="Alignment"/> used when drawing the te
+		/// <param name="alignment">Sets the <see cref="Alignment"/> used when drawing the text, <see cref="GTA.UI.Alignment.Left"/>,<see cref="GTA.UI.Alignment.Center"/> or <see cref="GTA.UI.Alignment.Right"/>.</param>
+		public TextElement(string caption, PointF position, float scale, Color color, Font font, Alignment alignment) :
+			this(caption, position, scale, color, font, alignment, false, false, 0.0f)
+		{
+		}
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TextElement"/> class used for drawing text on the screen.
+		/// </summary>
+		/// <param name="caption">The <see cref="TextElement"/> to draw.</param>
+		/// <param name="position">Set the <see cref="Position"/> on screen where to draw the <see cref="TextElement"/>.</param>
+		/// <param name="scale">Sets a <see cref="Scale"/> used to increase of decrease the size of the <see cref="TextElement"/>, for no scaling use 1.0f.</param>
+		/// <param name="color">Set the <see cref="Color"/> used to draw the <see cref="TextElement"/>.</param>
+		/// <param name="font">Sets the <see cref="Font"/> used when drawing the text.</param>
+		/// <param name="alignment">Sets the <see cref="Alignment"/> used when drawing the text, <see cref="GTA.UI.Alignment.Left"/>,<see cref="GTA.UI.Alignment.Center"/> or <see cref="GTA.UI.Alignment.Right"/>.</param>
+		/// <param name="shadow">Sets whether or not to draw the <see cref="TextElement"/> with a <see cref="Shadow"/> effect.</param>
+		/// <param name="outline">Sets whether or not to draw the <see cref="TextElement"/> with an <see cref="Outline"/> around the letters.</param>
+		public TextElement(string caption, PointF position, float scale, Color color, Font font, Alignment alignment, bool shadow, bool outline) :
+			this(caption, position, scale, color, font, alignment, shadow, outline, 0.0f)
+		{
+		}
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TextElement"/> class used for drawing text on the screen.
+		/// </summary>
+		/// <param name="caption">The <see cref="TextElement"/> to draw.</param>
+		/// <param name="position">Set the <see cref="Position"/> on screen where to draw the <see cref="TextElement"/>.</param>
+		/// <param name="scale">Sets a <see cref="Scale"/> used to increase of decre
